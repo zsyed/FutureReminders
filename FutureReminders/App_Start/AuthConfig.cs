36ed30da3fbf2a9424579b3a46f44c6cf68bd94d
zsyed <zulfiqarsyed@yahoo.com>
2013-10-21 05:57:25 -0700

    Added Elmah Error logging to the project.

--- FutureReminders/App_Start/AuthConfig.cs
+++ FutureReminders/App_Start/AuthConfig.cs
@@ -26,7 +26,7 @@ namespace FutureReminders
             //    appId: "",
             //    appSecret: "");
 
-            //OAuthWebSecurity.RegisterGoogleClient();
+            OAuthWebSecurity.RegisterGoogleClient();
         }
     }
 }
---
+++ FutureReminders/InitSecurityDb.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using System.Web.Security;
+using FutureReminders.Models;
+using WebMatrix.WebData;
+
+namespace SeedSimple
+{
+    public class InitSecurityDb : DropCreateDatabaseIfModelChanges<UsersContext>
+    {
+        protected override void Seed(UsersContext context)
+        {
+
+            WebSecurity.InitializeDatabaseConnection("DefaultConnection",
+               "UserProfile", "UserId", "UserName", autoCreateTables: true);
+            var roles = (SimpleRoleProvider)Roles.Provider;
+            var membership = (SimpleMembershipProvider)Membership.Provider;
+
+            if (!roles.RoleExists("Admin"))
+            {
+                roles.CreateRole("Admin");
+            }
+            if (membership.GetUser("test", false) == null)
+            {
+                membership.CreateUserAndAccount("test", "test");
+            }
+            if (!roles.GetRolesForUser("test").Contains("Admin"))
+            {
+                roles.AddUsersToRoles(new[] { "test" }, new[] { "admin" });
+            }
+
+        }
+    }
+}
