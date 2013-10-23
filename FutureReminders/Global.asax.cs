696f96900a71f3423a69cbf7aeed0ba36c502f60
zsyed <zulfiqarsyed@yahoo.com>
2013-10-22 21:51:05 -0700

    Added a line in global.asax that  to ignore FutureRemindersDb ..

--- FutureReminders/Global.asax.cs
+++ FutureReminders/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using FutureReminders.Models;
 
 namespace FutureReminders
 {
@@ -16,6 +18,8 @@ namespace FutureReminders
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<FutureRemindersDb>(null);
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
